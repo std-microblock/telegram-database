40438a86b615b96c4d5a772c070840f9f8c67ee6
star9029 <hengxings783@gmail.com>
2025-05-23 19:31:08 +0800

    ci: fix tdlib

--- xmake.lua
+++ xmake.lua
@@ -4,35 +4,39 @@ set_policy("compatibility.version", "3.0")
 includes("dependencies/tdlib.lua")
 includes("dependencies/yalantinglibs.lua")
 
-add_requires("tdlib", "faiss", "rocksdb", "yalantinglibs", {
-    configs = { ssl = true }
-}, "gtest", "benchmark", "reflect-cpp", "utfcpp")
+option("test", {description = "Enable test", default = false, type = "boolean"})
+
+add_requires("tdlib", "faiss", "rocksdb", "yalantinglibs", "reflect-cpp", "utfcpp")
+if has_config("test") then
+    add_requires("gtest", "benchmark")
+end
+
 set_languages("c++2b")
-set_warnings("all") 
+set_warnings("all")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
 add_rules("mode.releasedbg")
--- set_toolset("ld","lld-link.exe")
 
+if is_plat("windows") then
+    add_defines("NOMINMAX")
+end
 
 target("tgdb")
     set_kind("binary")
     set_encodings("utf-8")
-    add_defines("NOMINMAX")
     add_packages("tdlib", "faiss", "rocksdb", "yalantinglibs", "reflect-cpp", "utfcpp")
     add_files("src/*.cc", "src/*/**.cc") 
-    set_languages("c++2b")
 
 target("database_test")
+    set_default(false)
     set_kind("binary")
-    add_defines("NOMINMAX")
     add_files("test/database_test.cc")
     add_packages("gtest", "rocksdb", "yalantinglibs", "benchmark")
-    set_languages("c++2b")
+    add_tests("default")
 
 target("vector_db_test")
+    set_default(false)
     set_kind("binary")
-    add_defines("NOMINMAX")
     add_files("test/vector_db_test.cc", "src/database/faiss_vector_db.cc") 
     add_includedirs("src/database") 
     add_packages("gtest", "faiss", "yalantinglibs") 
-    set_languages("c++2b")
+    add_tests("default")
